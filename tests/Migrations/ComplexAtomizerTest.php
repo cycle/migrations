523872b51d8d05f008f11350ed700682c0baee10
Wolfy-J <wolfy.jd@gmail.com>
2018-09-29 17:19:17 +0300

    testing isolated issue on travis

--- tests/Migrations/ComplexAtomizerTest.php
+++ tests/Migrations/ComplexAtomizerTest.php
@@ -8,163 +8,162 @@
 
 namespace Spiral\Migrations\Tests;
 
-
 use Spiral\Database\ForeignKeyInterface;
 
 abstract class ComplexAtomizerTest extends BaseTest
 {
-    public function testCreateMultiple()
-    {
-        //Create thought migration
-        $this->migrator->configure();
-
-        $schema = $this->schema('sample');
-        $schema->primary('id');
-        $schema->integer('value');
-        $schema->index(['value']);
-
-        $schema1 = $this->schema('sample1');
-        $schema1->primary('id');
-        $schema1->float('value');
-        $schema1->integer('sample_id');
-        $schema1->foreignKey('sample_id')->references('sample', 'id');
-
-        $this->atomize('migration1', [$schema, $schema1]);
-        $this->migrator->run();
-
-        $this->assertTrue($this->db->hasTable('sample'));
-        $this->assertTrue($this->db->hasTable('sample1'));
-
-        $this->migrator->rollback();
-        $this->assertFalse($this->db->hasTable('sample'));
-        $this->assertFalse($this->db->hasTable('sample1'));
-    }
-
-    public function testCreateMultipleChangeFK()
-    {
-        //Create thought migration
-        $this->migrator->configure();
-
-        $schema = $this->schema('sample');
-        $schema->primary('id');
-        $schema->integer('value');
-        $schema->index(['value']);
-
-        $schema1 = $this->schema('sample1');
-        $schema1->primary('id');
-        $schema1->float('value');
-        $schema1->integer('sample_id');
-        $schema1->foreignKey('sample_id')->references('sample', 'id')
-            ->onDelete(ForeignKeyInterface::CASCADE)
-            ->onUpdate(ForeignKeyInterface::CASCADE);
-
-        $this->atomize('migration1', [$schema, $schema1]);
-        $this->migrator->run();
-
-        $this->assertTrue($this->db->hasTable('sample'));
-        $this->assertTrue($this->db->hasTable('sample1'));
-
-        $fk = $this->schema('sample1')->foreignKey('sample_id');
-        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getDeleteRule());
-        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getUpdateRule());
-
-        $schema1 = $this->schema('sample1');
-        $schema1->foreignKey('sample_id')->references('sample', 'id')
-            ->onDelete(ForeignKeyInterface::NO_ACTION)
-            ->onUpdate(ForeignKeyInterface::NO_ACTION);
-
-        $this->atomize('migration1', [$this->schema('sample'), $schema1]);
-        $this->migrator->run();
-
-        $fk = $this->schema('sample1')->foreignKey('sample_id');
-        $this->assertSame(ForeignKeyInterface::NO_ACTION, $fk->getDeleteRule());
-        $this->assertSame(ForeignKeyInterface::NO_ACTION, $fk->getUpdateRule());
-
-        $this->migrator->rollback();
-        $this->assertTrue($this->db->hasTable('sample'));
-        $this->assertTrue($this->db->hasTable('sample1'));
-
-        $fk = $this->schema('sample1')->foreignKey('sample_id');
-        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getDeleteRule());
-        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getUpdateRule());
-
-        $this->migrator->rollback();
-        $this->assertFalse($this->db->hasTable('sample'));
-        $this->assertFalse($this->db->hasTable('sample1'));
-    }
-
-    public function testCreateMultipleWithPivot()
-    {
-        //Create thought migration
-        $this->migrator->configure();
-
-        $schema = $this->schema('sample');
-        $schema->primary('id');
-        $schema->integer('value');
-        $schema->index(['value']);
-
-        $schema1 = $this->schema('sample1');
-        $schema1->primary('id');
-        $schema1->float('value');
-        $schema1->integer('sample_id');
-        $schema1->foreignKey('sample_id')->references('sample', 'id');
-
-        $schema2 = $this->schema('sample2');
-        $schema2->integer('sample_id');
-        $schema2->foreignKey('sample_id')->references('sample', 'id');
-        $schema2->integer('sample1_id');
-        $schema2->foreignKey('sample1_id')->references('sample1', 'id');
-
-        $this->atomize('migration1', [$schema, $schema1, $schema2]);
-        $this->migrator->run();
-
-        $this->assertTrue($this->db->hasTable('sample'));
-        $this->assertTrue($this->db->hasTable('sample1'));
-        $this->assertTrue($this->db->hasTable('sample2'));
-
-        $this->migrator->rollback();
-        $this->assertFalse($this->db->hasTable('sample'));
-        $this->assertFalse($this->db->hasTable('sample1'));
-        $this->assertFalse($this->db->hasTable('sample2'));
-    }
-
-    public function testCreateAndAddFK()
-    {
-        //Create thought migration
-        $this->migrator->configure();
-
-        $schema = $this->schema('sample');
-        $schema->primary('id');
-        $schema->integer('value');
-        $schema->index(['value']);
-
-        $schema1 = $this->schema('sample1');
-        $schema1->primary('id');
-        $schema1->float('value');
-
-        $this->atomize('migration1', [$schema, $schema1]);
-        $this->migrator->run();
-        $this->assertTrue($this->db->hasTable('sample'));
-        $this->assertTrue($this->db->hasTable('sample1'));
-
-        $schema1 = $this->schema('sample1');
-        $schema1->integer('sample_id');
-        $schema1->foreignKey('sample_id')->references('sample', 'id');
-
-        $this->atomize('migration2', [$this->schema('sample'), $schema1]);
-
-        $this->migrator->run();
-        $this->assertTrue($this->db->hasTable('sample'));
-        $this->assertTrue($this->db->hasTable('sample1'));
-        $this->assertTrue($this->schema('sample1')->hasForeignKey('sample_id'));
-
-        $this->migrator->rollback();
-        $this->assertTrue($this->db->hasTable('sample'));
-        $this->assertTrue($this->db->hasTable('sample1'));
-        $this->assertFalse($this->schema('sample1')->hasForeignKey('sample_id'));
-
-        $this->migrator->rollback();
-        $this->assertFalse($this->db->hasTable('sample'));
-        $this->assertFalse($this->db->hasTable('sample1'));
-    }
+//    public function testCreateMultiple()
+//    {
+//        //Create thought migration
+//        $this->migrator->configure();
+//
+//        $schema = $this->schema('sample');
+//        $schema->primary('id');
+//        $schema->integer('value');
+//        $schema->index(['value']);
+//
+//        $schema1 = $this->schema('sample1');
+//        $schema1->primary('id');
+//        $schema1->float('value');
+//        $schema1->integer('sample_id');
+//        $schema1->foreignKey('sample_id')->references('sample', 'id');
+//
+//        $this->atomize('migration1', [$schema, $schema1]);
+//        $this->migrator->run();
+//
+//        $this->assertTrue($this->db->hasTable('sample'));
+//        $this->assertTrue($this->db->hasTable('sample1'));
+//
+//        $this->migrator->rollback();
+//        $this->assertFalse($this->db->hasTable('sample'));
+//        $this->assertFalse($this->db->hasTable('sample1'));
+//    }
+//
+//    public function testCreateMultipleChangeFK()
+//    {
+//        //Create thought migration
+//        $this->migrator->configure();
+//
+//        $schema = $this->schema('sample');
+//        $schema->primary('id');
+//        $schema->integer('value');
+//        $schema->index(['value']);
+//
+//        $schema1 = $this->schema('sample1');
+//        $schema1->primary('id');
+//        $schema1->float('value');
+//        $schema1->integer('sample_id');
+//        $schema1->foreignKey('sample_id')->references('sample', 'id')
+//            ->onDelete(ForeignKeyInterface::CASCADE)
+//            ->onUpdate(ForeignKeyInterface::CASCADE);
+//
+//        $this->atomize('migration1', [$schema, $schema1]);
+//        $this->migrator->run();
+//
+//        $this->assertTrue($this->db->hasTable('sample'));
+//        $this->assertTrue($this->db->hasTable('sample1'));
+//
+//        $fk = $this->schema('sample1')->foreignKey('sample_id');
+//        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getDeleteRule());
+//        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getUpdateRule());
+//
+//        $schema1 = $this->schema('sample1');
+//        $schema1->foreignKey('sample_id')->references('sample', 'id')
+//            ->onDelete(ForeignKeyInterface::NO_ACTION)
+//            ->onUpdate(ForeignKeyInterface::NO_ACTION);
+//
+//        $this->atomize('migration1', [$this->schema('sample'), $schema1]);
+//        $this->migrator->run();
+//
+//        $fk = $this->schema('sample1')->foreignKey('sample_id');
+//        $this->assertSame(ForeignKeyInterface::NO_ACTION, $fk->getDeleteRule());
+//        $this->assertSame(ForeignKeyInterface::NO_ACTION, $fk->getUpdateRule());
+//
+//        $this->migrator->rollback();
+//        $this->assertTrue($this->db->hasTable('sample'));
+//        $this->assertTrue($this->db->hasTable('sample1'));
+//
+//        $fk = $this->schema('sample1')->foreignKey('sample_id');
+//        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getDeleteRule());
+//        $this->assertSame(ForeignKeyInterface::CASCADE, $fk->getUpdateRule());
+//
+//        $this->migrator->rollback();
+//        $this->assertFalse($this->db->hasTable('sample'));
+//        $this->assertFalse($this->db->hasTable('sample1'));
+//    }
+//
+//    public function testCreateMultipleWithPivot()
+//    {
+//        //Create thought migration
+//        $this->migrator->configure();
+//
+//        $schema = $this->schema('sample');
+//        $schema->primary('id');
+//        $schema->integer('value');
+//        $schema->index(['value']);
+//
+//        $schema1 = $this->schema('sample1');
+//        $schema1->primary('id');
+//        $schema1->float('value');
+//        $schema1->integer('sample_id');
+//        $schema1->foreignKey('sample_id')->references('sample', 'id');
+//
+//        $schema2 = $this->schema('sample2');
+//        $schema2->integer('sample_id');
+//        $schema2->foreignKey('sample_id')->references('sample', 'id');
+//        $schema2->integer('sample1_id');
+//        $schema2->foreignKey('sample1_id')->references('sample1', 'id');
+//
+//        $this->atomize('migration1', [$schema, $schema1, $schema2]);
+//        $this->migrator->run();
+//
+//        $this->assertTrue($this->db->hasTable('sample'));
+//        $this->assertTrue($this->db->hasTable('sample1'));
+//        $this->assertTrue($this->db->hasTable('sample2'));
+//
+//        $this->migrator->rollback();
+//        $this->assertFalse($this->db->hasTable('sample'));
+//        $this->assertFalse($this->db->hasTable('sample1'));
+//        $this->assertFalse($this->db->hasTable('sample2'));
+//    }
+//
+//    public function testCreateAndAddFK()
+//    {
+//        //Create thought migration
+//        $this->migrator->configure();
+//
+//        $schema = $this->schema('sample');
+//        $schema->primary('id');
+//        $schema->integer('value');
+//        $schema->index(['value']);
+//
+//        $schema1 = $this->schema('sample1');
+//        $schema1->primary('id');
+//        $schema1->float('value');
+//
+//        $this->atomize('migration1', [$schema, $schema1]);
+//        $this->migrator->run();
+//        $this->assertTrue($this->db->hasTable('sample'));
+//        $this->assertTrue($this->db->hasTable('sample1'));
+//
+//        $schema1 = $this->schema('sample1');
+//        $schema1->integer('sample_id');
+//        $schema1->foreignKey('sample_id')->references('sample', 'id');
+//
+//        $this->atomize('migration2', [$this->schema('sample'), $schema1]);
+//
+//        $this->migrator->run();
+//        $this->assertTrue($this->db->hasTable('sample'));
+//        $this->assertTrue($this->db->hasTable('sample1'));
+//        $this->assertTrue($this->schema('sample1')->hasForeignKey('sample_id'));
+//
+//        $this->migrator->rollback();
+//        $this->assertTrue($this->db->hasTable('sample'));
+//        $this->assertTrue($this->db->hasTable('sample1'));
+//        $this->assertFalse($this->schema('sample1')->hasForeignKey('sample_id'));
+//
+//        $this->migrator->rollback();
+//        $this->assertFalse($this->db->hasTable('sample'));
+//        $this->assertFalse($this->db->hasTable('sample1'));
+//    }
 }
--- tests/Migrations/MigratorTest.php
+++ tests/Migrations/MigratorTest.php
@@ -11,49 +11,49 @@ namespace Spiral\Migrations\Tests;
 
 abstract class MigratorTest extends BaseTest
 {
-    public function testConfigure()
-    {
-        $this->assertFalse($this->migrator->isConfigured());
-
-        $this->migrator->configure();
-        $this->assertTrue($this->db->hasTable('migrations'));
-    }
-
-    //no errors expected
-    public function testConfigureTwice()
-    {
-        $this->assertFalse($this->migrator->isConfigured());
-
-        $this->migrator->configure();
-        $this->assertTrue($this->db->hasTable('migrations'));
-
-        $this->migrator->configure();
-    }
-
-    public function testGetEmptyMigrations()
-    {
-        $this->migrator->configure();
-        $this->assertSame([], $this->migrator->getMigrations());
-    }
-
-    public function testRepository()
-    {
-        $this->assertSame($this->repository, $this->migrator->getRepository());
-    }
-
-    /**
-     * @expectedException \Spiral\Migrations\Exception\MigrationException
-     */
-    public function testRunUnconfigured()
-    {
-        $this->migrator->run();
-    }
-
-    /**
-     * @expectedException \Spiral\Migrations\Exception\MigrationException
-     */
-    public function testRollbackUnconfigured()
-    {
-        $this->migrator->rollback();
-    }
+//    public function testConfigure()
+//    {
+//        $this->assertFalse($this->migrator->isConfigured());
+//
+//        $this->migrator->configure();
+//        $this->assertTrue($this->db->hasTable('migrations'));
+//    }
+//
+//    //no errors expected
+//    public function testConfigureTwice()
+//    {
+//        $this->assertFalse($this->migrator->isConfigured());
+//
+//        $this->migrator->configure();
+//        $this->assertTrue($this->db->hasTable('migrations'));
+//
+//        $this->migrator->configure();
+//    }
+//
+//    public function testGetEmptyMigrations()
+//    {
+//        $this->migrator->configure();
+//        $this->assertSame([], $this->migrator->getMigrations());
+//    }
+//
+//    public function testRepository()
+//    {
+//        $this->assertSame($this->repository, $this->migrator->getRepository());
+//    }
+//
+//    /**
+//     * @expectedException \Spiral\Migrations\Exception\MigrationException
+//     */
+//    public function testRunUnconfigured()
+//    {
+//        $this->migrator->run();
+//    }
+//
+//    /**
+//     * @expectedException \Spiral\Migrations\Exception\MigrationException
+//     */
+//    public function testRollbackUnconfigured()
+//    {
+//        $this->migrator->rollback();
+//    }
 }
