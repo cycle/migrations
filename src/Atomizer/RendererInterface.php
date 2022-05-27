3ec16dece8f1aeb1fdefb7bb5cacf2729cd6c97c
Maxim Smakouz <m.smakouz@gmail.com>
2022-05-27 18:42:25 +0300

    Migrate Atomizer to a new Reactor

--- src/Atomizer/RendererInterface.php
+++ src/Atomizer/RendererInterface.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Cycle\Migrations\Atomizer;
 
 use Cycle\Database\Schema\AbstractTable;
-use Spiral\Reactor\Partial\Source;
+use Spiral\Reactor\Partial\Method;
 
 /**
  * Renders table differences and create syntaxes into given source.
@@ -15,20 +15,20 @@ interface RendererInterface
     /**
      * Migration engine specific table creation syntax.
      */
-    public function createTable(Source $source, AbstractTable $table);
+    public function createTable(Method $method, AbstractTable $table);
 
     /**
      * Migration engine specific table update syntax.
      */
-    public function updateTable(Source $source, AbstractTable $table);
+    public function updateTable(Method $method, AbstractTable $table);
 
     /**
      * Migration engine specific table revert syntax.
      */
-    public function revertTable(Source $source, AbstractTable $table);
+    public function revertTable(Method $method, AbstractTable $table);
 
     /**
      * Migration engine specific table drop syntax.
      */
-    public function dropTable(Source $source, AbstractTable $table);
+    public function dropTable(Method $method, AbstractTable $table);
 }
