e3f52333296a983ba88b959f41ada026fabd57df
Maxim Smakouz <m.smakouz@gmail.com>
2023-05-15 11:32:59 +0300

    Fix method name

--- src/Atomizer/Renderer.php
+++ src/Atomizer/Renderer.php
@@ -282,7 +282,7 @@ final class Renderer implements RendererInterface
             'name' => $reference->getName(),
             'delete' => $reference->getDeleteRule(),
             'update' => $reference->getUpdateRule(),
-            'indexCreate' => $reference->shouldCreateIndex(),
+            'indexCreate' => $reference->hasIndex(),
         ];
     }
 
