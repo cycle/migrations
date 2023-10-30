05d9c5ddbb989d56d24b19bae5dcce4bf3f9844c
Maxim Smakouz <m.smakouz@gmail.com>
2023-10-30 15:14:12 +0200

    Add TableSorter

---
+++ src/Atomizer/TableSorter.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\Migrations\Atomizer;
+
+use Cycle\Database\Schema\AbstractTable;
+use Cycle\Database\Schema\Reflector;
+
+final class TableSorter
+{
+    /**
+     * Tables sorted in order of their dependencies.
+     *
+     * @param AbstractTable[] $tables
+     *
+     * @return AbstractTable[]
+     */
+    public function sort(array $tables, $reverse = false): array
+    {
+        $reflector = new Reflector();
+        foreach ($tables as $table) {
+            $reflector->addTable($table);
+        }
+
+        $sorted = $reflector->sortedTables();
+        if ($reverse) {
+            return \array_reverse($sorted);
+        }
+
+        return $sorted;
+    }
+}
