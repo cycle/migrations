87ea50bee67b8d2b9f22850d3f3a3d73c9e6b693
Maxim Smakouz <m.smakouz@gmail.com>
2023-10-30 15:18:28 +0200

    Add unit test

--- tests/Migrations/TableSorterTest.php
+++ tests/Migrations/TableSorterTest.php
@@ -36,4 +36,19 @@ abstract class TableSorterTest extends BaseTest
 
         $this->assertSame([$table1, $table2], $sorter->sort([$table1, $table2]));
     }
+
+    public function testSortWithIncorrectOrder(): void
+    {
+        $sorter = new TableSorter();
+        $table1 = $this->schema('table1');
+        $table1->primary('id');
+        $table1->integer('sample_id');
+        $table1->foreignKey(['sample_id'])->references('table2', ['id']);
+
+        $table2 = $this->schema('table2');
+        $table2->primary('id');
+        $table2->integer('value');
+
+        $this->assertSame([$table2, $table1], $sorter->sort([$table1, $table2]));
+    }
 }
