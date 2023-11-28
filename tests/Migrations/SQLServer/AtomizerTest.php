ef49ac2113c4b727448f55aa2851d225f4641a1a
Maxim Smakouz <m.smakouz@gmail.com>
2023-11-28 17:03:42 +0200

    Add the ability to define size for binary

--- tests/Migrations/SQLServer/AtomizerTest.php
+++ tests/Migrations/SQLServer/AtomizerTest.php
@@ -14,4 +14,35 @@ namespace Cycle\Migrations\Tests\SQLServer;
 class AtomizerTest extends \Cycle\Migrations\Tests\AtomizerTest
 {
     public const DRIVER = 'sqlserver';
+
+    public function testChangeBinaryColumnSize(): void
+    {
+        $this->migrator->configure();
+
+        $schema = $this->schema('sample');
+        $schema->primary('id');
+        $schema->binary('value', size: 16);
+        $this->atomize('migration1', [$schema]);
+
+        $this->migrator->run();
+        $this->assertSame(16, $this->schema('sample')->column('value')->getSize());
+
+        $schema = $this->schema('sample');
+        $schema->binary('value', size: 255);
+        $this->atomize('migration2', [$schema]);
+
+        $this->migrator->run();
+
+        $this->assertSame(255, $this->schema('sample')->column('value')->getSize());
+
+        $this->assertTrue($this->db->hasTable('sample'));
+
+        $this->migrator->rollback();
+        $this->assertSame(16, $this->schema('sample')->column('value')->getSize());
+
+        $this->assertTrue($this->db->hasTable('sample'));
+
+        $this->migrator->rollback();
+        $this->assertFalse($this->db->hasTable('sample'));
+    }
 }
